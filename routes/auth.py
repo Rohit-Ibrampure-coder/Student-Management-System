233dc1438ec4019c7a9247b67f16fd3925a5c6be
Rohit Ibrampure <ibrampurerohit043@gmail.com>
2026-06-17 13:45:18 +0530

    Implemented User Registration
    Added password hashing and duplicate email validation

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,41 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from werkzeug.security import generate_password_hash
+from models.user import User
+from extensions import db
+from models.user import User
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/signup")
+@auth.route("/signup", methods=["GET", "POST"])
 def signup():
+
+    if request.method == "POST":
+
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+
+        if password != confirm_password:
+            return "Passwords do not match!"
+
+        # Check existing email
+        existing_user = User.query.filter_by(email=email).first()
+
+        if existing_user:
+            return "Email already registered!"
+
+        hashed_password = generate_password_hash(password)
+
+        new_user = User(
+            name=name,
+            email=email,
+            password=hashed_password
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return "User Registered Successfully!"
+
     return render_template("signup.html")
